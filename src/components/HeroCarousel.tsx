18e62db9d7843cb9713f852036b280a54f9eacbb
danielsousast <daniel.sousa.developer@gmail.com>
2025-09-16 13:20:23 -0300

    feat: implement dark mode
    - Implement dark mode on all elements

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -53,7 +53,7 @@ export default function HeroCarousel() {
   ]
 
   return (
-    <section className="relative sm:pt-18 px-4 sm:pl-8 sm:pr-0">
+    <section className="relative dark:bg-neutral-900 sm:pt-18 px-4 sm:pl-8 sm:pr-0">
 
 
 
@@ -128,14 +128,14 @@ export default function HeroCarousel() {
 
         {/* Swiper buttons */}
         <div className="flex absolute justify-between w-full sm:justify-center sm:w-fit bottom-[0] sm:right-8 gap-2 z-10">
-          <button className={`custom-swiper-prev z-10 p-2 sm:p-3 bg-gray-100 hover:bg-white hover:shadow-md rounded-full transition-all cursor-pointer`}>
-            <svg className="size-5 sm:size-4 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+          <button className={`custom-swiper-prev z-10 p-2 sm:p-3 dark:bg-neutral-700 dark:hover:bg-neutral-600 bg-gray-100 hover:bg-white hover:shadow-md rounded-full transition-all cursor-pointer`}>
+            <svg className="size-5 sm:size-4 dark:text-white text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
             </svg>
           </button>
 
-          <button className={`custom-swiper-next z-10 p-2 sm:p-3 bg-gray-100 hover:bg-white hover:shadow-md rounded-full transition-all cursor-pointer`}>
-            <svg className="size-5 sm:size-4 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+          <button className={`custom-swiper-next z-10 p-2 sm:p-3 dark:bg-neutral-700 dark:hover:bg-neutral-600 bg-gray-100 hover:bg-white hover:shadow-md rounded-full transition-all cursor-pointer`}>
+            <svg className="size-5 sm:size-4 dark:text-white text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
             </svg>
           </button>
